2efdf08e0cc6c16f740c919601dd98d29962c48f
AlexSheer <alex_sheer@inbox.lv>
2018-04-30 21:26:15 +0300

    Add files via upload

---
+++ migrations/version_1_0_7.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ *
+ * Knowledge base. An extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2017, Sheer
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace sheer\knowledgebase\migrations;
+
+class version_1_0_7 extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['knowledge_base_version']) && version_compare($this->config['knowledge_base_version'], '1.0.7', '>=');
+	}
+
+	static public function depends_on()
+	{
+		return array('\sheer\knowledgebase\migrations\version_1_0_6');
+	}
+
+	public function update_schema()
+	{
+		return array();
+	}
+
+
+	public function revert_schema()
+	{
+		return array();
+	}
+
+	public function update_data()
+	{
+		return array(
+			// Update configs
+			array('config.update', array('knowledge_base_version', '1.0.7')),
+			// Add permissions
+			array('permission.add', array('u_kb_view', true)),
+		);
+	}
+}
