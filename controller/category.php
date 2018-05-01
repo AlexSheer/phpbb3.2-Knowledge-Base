add385cbd51a426246d78153dfd6ef869f70c95b
AlexSheer <alex_sheer@inbox.lv>
2018-05-01 23:09:35 +0300

    Update category.php

--- controller/category.php
+++ controller/category.php
@@ -77,7 +77,7 @@ class category
 
 	public function cat()
 	{
-		if (!$this->auth->acl_get('u_kb_view') || !$this->auth->acl_get('a_manage_kb'))
+		if (!$this->auth->acl_get('u_kb_view') && !$this->auth->acl_get('a_manage_kb'))
 		{
 			trigger_error($this->user->lang['NOT_AUTHORISED']);
 		}
